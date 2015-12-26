816bdab818b232319a6a32e8714e0102148d4ac5
Ben Copeland <bscopeland@gmail.com>
2015-12-26 09:37:24 -0500

    Added ball sprite

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,14 +1,23 @@
-"use strict";
+'use strict';
 
-var GAMe = new Phaser.Game(480, 320, Phaser.AUTO, null, {
+// Define game object and assign preload, create, and update objects
+var GAME = new Phaser.Game(480, 320, Phaser.AUTO, null, {
   preload: preload, create: create, update: update
 });
+
+// Ball settings
+var ball = undefined;
+
 function preload() {
   GAME.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
   GAME.scale.pageAlignHorizontally = true;
   GAME.scale.pageAlignVertically = true;
+  GAME.stage.backgroundColor = '#eee';
+  GAME.load.image('ball', '../src/img/ball.png');
 }
 
-function create() {}
+function create() {
+  ball = GAME.add.sprite(50, 50, 'ball');
+}
 
 function update() {}
--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,22 @@
 
-const GAMe = new Phaser.Game(480, 320, Phaser.AUTO, null, {
+// Define game object and assign preload, create, and update objects
+const GAME = new Phaser.Game(480, 320, Phaser.AUTO, null, {
   preload: preload, create: create, update: update
 });
+
+// Ball settings
+let ball;
+
 function preload() {
   GAME.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
   GAME.scale.pageAlignHorizontally = true;
   GAME.scale.pageAlignVertically = true;
+  GAME.stage.backgroundColor = '#eee';
+  GAME.load.image('ball', '../src/img/ball.png');
 }
 
-function create() {}
+function create() {
+  ball = GAME.add.sprite(50, 50, 'ball');
+}
 
 function update() {} 
